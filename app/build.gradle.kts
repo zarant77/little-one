fc2355a15b9adb383e6453d11baaf546b6c822d7
Anton Zarubin <anton.zarubin@gmail.com>
2026-07-22 12:11:27 +0300

    v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.catemup.littleone"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.4.0"
 
         ndk {
             abiFilters += "arm64-v8a"
